e81f81c0279b0ed6f88d9f3f6b6fc530c112f6df
Norazan <thijs.hendrickx@online.nl>
2016-01-15 12:50:27 +0100

    Added WashingMachineStatus checks in WashingProgramController.

--- src/WashingProgramController.cpp
+++ src/WashingProgramController.cpp
@@ -21,22 +21,53 @@ WashingProgramController::WashingProgramController(
 void WashingProgramController::startWashingProgram(WashingProgram program){
 	door.lock();
 	washingMachine.start();
-	scheduler = WashingProgramScheduler(this);
-	scheduler.startWashingProgram(program);
+	washingMachineStatusSensor.update();
+	updateStatusSensorTimer.set(50);
+	wait(updateStatusSensorTimer);
+	if(washingMachineStatus == IDLE){
+		scheduler = WashingProgramScheduler(this);
+		scheduler.startWashingProgram(program);
+	}
+	else{
+		std::cout << "Status is: " << washingMachineStatus << ". Did not start washing program." << endl;
+	}
 }
 
 void WashingProgramController::stopWashingProgram(){
 	washingMachine.stop();
 	door.unlock();
+	washingMachineStatusSensor.update();
+	updateStatusSensorTimer.set(50);
+	wait(updateStatusSensorTimer);
+	if(washingMachineStatus == STOPPED){
+		scheduler.stopWashingProgram();
+	}
+	else{
+		std::cout << "Washing Machine not stopped. Status: " << washingMachineStatus << endl;
+	}
+}
+
+void WashingProgramController::valueChanged(HardwareSensor* sensor, unsigned char value){
+	if(sensor == washingMachineStatusSensor){
+		washingMachineStatus = value;
+	}
 }
 
 void WashingProgramController::main(){
 	RTOS::timer washingProgramTimer(this, "stepTimer");
 	while(true){
-		waterLevelController.setGoalState(scheduler.getCurrentStep().waterLevel);
-		temperatureController.setGoalState(scheduler.getCurrentStep().temperature);
-		rotationController.setGoalState(scheduler.getCurrentStep().rotationSpeed);
-		scheduler.update();
+		if(washingMachineStatus == RUNNING){
+			if(scheduler.isRunning()){
+				waterLevelController.setGoalState(scheduler.getCurrentStep().waterLevel);
+				temperatureController.setGoalState(scheduler.getCurrentStep().temperature);
+				rotationController.setGoalState(scheduler.getCurrentStep().rotationSpeed);
+			}
+			scheduler.update();
+		}
+		else{
+			std::cout << "Washing Machine not running. Status: " << washingMachineStatus << endl;
+		}
+		washingMachineStatusSensor.update();
 		washingProgramTimer.set(1000);
 		wait(washingProgramTimer);
 	}
