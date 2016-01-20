de37cddf350d3f4595b3469acf04cc6429edc6d2
Norazan <thijs.hendrickx@online.nl>
2016-01-20 10:38:30 +0100

    Added documentation for boundary classes

--- src/HardwareComponent.hpp
+++ src/HardwareComponent.hpp
@@ -8,13 +8,13 @@
 
 //Requests
 
-#define MACHINE_REQ 		0x01
+#define MACHINE_REQ 			0x01
 #define DOOR_LOCK_REQ 		0x02
 #define WATER_VALVE_REQ		0x03
 #define SOAP_DISPENSER_REQ	0x04
-#define PUMP_REQ			0x05
+#define PUMP_REQ				0x05
 #define WATER_LEVEL_REQ		0x06
-#define HEATING_UNIT_REQ	0x07
+#define HEATING_UNIT_REQ		0x07
 #define TEMPERATURE_REQ		0x08
 #define SET_RPM_REQ			0x0A
 #define GET_RPM_REQ			0x09
@@ -24,33 +24,33 @@
 
 #define STATUS_CMD			0x01
 
-#define START_CMD			0x10
-#define STOP_CMD			0x20
+#define START_CMD				0x10
+#define STOP_CMD				0x20
 
-#define LOCK_CMD			0x40
+#define LOCK_CMD				0x40
 #define UNLOCK_CMD			0x80
 
-#define OPEN_CMD			0x10
-#define CLOSE_CMD			0x20
+#define OPEN_CMD				0x10
+#define CLOSE_CMD				0x20
 
-#define ON_CMD				0x10
+#define ON_CMD					0x10
 #define OFF_CMD				0x20
 
-#define EMPTY_CMD			0x00
+#define EMPTY_CMD				0x00
 
 //Answers
 
-#define HALTED				0x01
-#define IDLE				0x02
+#define HALTED					0x01
+#define IDLE					0x02
 #define RUNNING				0x04
 #define STOPPED				0x08
-#define FAILED				0xFF
+#define FAILED					0xFF
 
-#define OPENED				0x01
-#define CLOSED				0x02
-#define LOCKED				0x04
+#define OPENED					0x01
+#define CLOSED					0x02
+#define LOCKED					0x04
 
-#define ON					0x08
+#define ON						0x08
 #define OFF					0x10
 
 /*---------------------------------*/
--- src/Heating.hpp
+++ src/Heating.hpp
@@ -2,10 +2,28 @@
 
 #include "HardwareComponent.hpp"
 
+/**
+ * @class Heating
+ * @author Bouke Stam
+ * @date 20/01/16
+ * @file Heating.hpp
+ * @brief Boundary class for the washing machine Heating unit
+ */
+
 class Heating: public HardwareComponent{
 public:
+/**
+ * @brief Constructor for Heating
+ */
 	Heating(UARTInterface* uartInterface);
 
+/**
+ * @brief Turns the heating unit on
+ */
 	void on();
+
+/**
+ * @brief Turns the heating unit off
+ */
 	void off();
 };
