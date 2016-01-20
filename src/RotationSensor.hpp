cca22d956fe9273d1e500478f47ba1c35cfd3956
Norazan <thijs.hendrickx@online.nl>
2016-01-20 11:56:24 +0100

    Added documentation to Hardware classes

--- src/RotationSensor.hpp
+++ src/RotationSensor.hpp
@@ -2,10 +2,29 @@
 
 #include "HardwareSensor.hpp"
 
+/**
+ * @class RotationSensor
+ * @author Bouke Stam
+ * @date 20/01/16
+ * @file RotationSensor.hpp
+ * @brief Boundary class for the Rotation Sensor in the washing machine
+ */
+
 class RotationSensor: public HardwareSensor{
 public:
+/**
+ * @brief Constructor for RotationSensor
+ */
 	RotationSensor(UARTInterface* uartInterface);
-	
+
+/**
+ * @brief Calls an update on the currentValue for TemperatureSensor
+ */
 	void update() override;
+
+/**
+ * @brief Gets called if UART recieves a response directed to this sensor
+ * @param response The message that was send by the UART
+ */
 	void responseReceived(UARTMessage response) override;
 };
