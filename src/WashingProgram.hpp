7464fc338f5aca7548ae1f781ce96056d043d2ce
Waila Woe <waila.woe@student.hu.nl>
2016-01-21 13:16:47 +0100

    edited document(doxyGen)

--- src/WashingProgram.hpp
+++ src/WashingProgram.hpp
@@ -11,13 +11,12 @@
  * @file WashingProgram.hpp
  * @brief A washing program with steps which is able to store values like rotationspeed, waterLevel, temperature and the duration a step.
  */
-
 class WashingProgram{
 public:
 	
 	/**
-	 * @brief returns the total duration of all steps combined
-	 * @return 
+	 * @brief Calculates the duration of all steps.
+	 * @return int::total which contains the total duration of steps
 	 */
 	int getTotalDuration();
 
@@ -25,22 +24,21 @@ public:
 	std::string dicription;
 	
 	/**
-	 * @brief returns step of the given index
-	 * @param index
-	 * @return 
+	 * @brief Retrieves the step of the given index
+	 * @param index An integer of the step
+	 * @return The WashingProgramStep of given index
 	 */
-
 	WashingProgramStep getStep(unsigned int index);
 	
 	/**
-	 * @brief returns the size of the vector steps
-	 * @return 
+	 * @brief  Retrieves size of the step 
+	 * @return Number of the total size of the vector steps
 	 */
 	size_t getStepSize();
 	
 	/**
-	 * @brief retrieves the given step and adds that step by pushing it to the vector:steps
-	 * @param step
+	 * @brief Retrieves the given step and adds that step by pushing it to the vector:steps
+	 * @param step Contains data of the given step
 	 */
 	void addStep(WashingProgramStep step);
 private:
